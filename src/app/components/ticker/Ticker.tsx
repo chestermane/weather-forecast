e26b699337a1718a0e4192e80cd1128fb57d9f23
Evan Chesterman <evanchesterman@Evans-Mac-mini.local>
2024-03-28 22:49:12 -0400

    - Adding (Ticker) component that scrolls related weather infomation on the screen

---
+++ src/app/components/ticker/Ticker.tsx
@@ -0,0 +1,24 @@
+import { WeatherContext } from "@/app/context/WeatherContext";
+import React, { useContext } from "react";
+import dayjs from "dayjs";
+
+import styles from "./Ticker.module.scss";
+
+function Ticker() {
+  const { period } = useContext(WeatherContext);
+  const currentDate = dayjs(period?.startTime).format("MM-DD-YY");
+  return (
+    <div style={{ margin: "0 9px" }}>
+      <ul className={styles.ticker}>
+        <li className={styles.ticketItem}>
+          Tomorrow&apos;s Forecast - {period?.detailedForecast}
+        </li>
+        <li aria-hidden className={styles.ticketItem}>
+          Tomorrows Forecast {period?.detailedForecast}
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Ticker;
